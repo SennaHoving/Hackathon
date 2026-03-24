e620df8144b08190bddcd969631afe11074a8838
SennaHoving <senna.hoving@gmail.com>
2026-03-24 11:31:47 +0100

    3d sphere module

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,51 @@
-const moduleComponents = document.querySelectorAll("#module div"); 
+/* imports */ 
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const options = {
-    root: null, 
-    rootMargin: "0px", 
-    threshold: 1, 
-}
+const moduleContainer = document.getElementById("module"); 
 
-const callback = (entries) => {
-    entries.forEach(entry => {  
-        const p = entry.target.querySelector("p"); 
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-        if(entry.isIntersecting) {
-            p.style.opacity = "1";
-        } else {
-            p.style.opacity = "0";
-        }
-    })
-}
+// Scene
+const scene = new THREE.Scene()
+// scene.background = new THREE.Color('#009398')
 
-const observer = new IntersectionObserver(callback, options);
+// Camera 
+const fov = 25; 
+const aspect = width / height;
+const near = 0.1; 
+const far = 100; 
 
-moduleComponents.forEach(component => {
-    observer.observe(component);
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); 
+camera.position.set(0, 0, 10); 
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true, 
 })
+const geometry = new THREE.IcosahedronGeometry(1.0, 2); 
+// const geometry = new THREE.BoxGeometry(2, 2, 2); 
+const cube = new THREE.Mesh(geometry, material); 
+scene.add(cube); 
+
+// Render
+const renderer = new THREE.WebGLRenderer(); 
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+moduleContainer.append(renderer.domElement); 
+renderer.render(scene, camera); 
+
+const controls = new OrbitControls(camera, renderer.domElement); 
+controls.enableDamping = true; 
+controls.enableZoom = false; 
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
