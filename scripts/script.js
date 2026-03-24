dd61e2b0c6089be350ffc729a5a778b910471a44
SennaHoving <senna.hoving@gmail.com>
2026-03-24 14:11:21 +0100

    3d cube

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 /* imports */ 
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const moduleContainer = document.getElementById("module"); 
 
@@ -23,14 +24,18 @@ const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); 
 camera.position.set(0, 0, 10); 
 
-const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true, 
-})
-const geometry = new THREE.IcosahedronGeometry(1.0, 2); 
-// const geometry = new THREE.BoxGeometry(2, 2, 2); 
-const cube = new THREE.Mesh(geometry, material); 
-scene.add(cube); 
+// 3D Shape 
+const loader = new GLTFLoader(); 
+loader.load( "../ass/cube.glb", function (gltf) {
+    const cube = gltf.scene
+
+    cube.traverse((child) => {
+        child.material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
+    });
+    
+    console.log( cube )
+    scene.add( cube ); 
+});
 
 // Render
 const renderer = new THREE.WebGLRenderer(); 
@@ -38,7 +43,7 @@ renderer.setSize(width, height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 moduleContainer.append(renderer.domElement); 
-renderer.render(scene, camera); 
+// renderer.render(scene, camera); 
 
 const controls = new OrbitControls(camera, renderer.domElement); 
 controls.enableDamping = true; 
@@ -48,18 +53,14 @@ function animate() {
     requestAnimationFrame(animate);
     controls.update();
 
-    if (cameraTargetPos && cameraTarget) {
+    if (cameraTargetPos) {
         // move camera position
         camera.position.lerp(cameraTargetPos, 0.05);
-        // move controls target
-        controls.target.lerp(cameraTarget, 0.05);
 
         // If close enough, stop moving
         if (camera.position.distanceTo(cameraTargetPos) < 0.01) {
             camera.position.copy(cameraTargetPos);
-            controls.target.copy(cameraTarget);
             cameraTargetPos = null;
-            cameraTarget = null;
         }
     }
 
@@ -68,22 +69,19 @@ function animate() {
 
 animate();
 
-function moveCameraTo(newPosition, newLook) {
+function moveCameraTo(newPosition) {
     cameraTargetPos = newPosition.clone(); 
-    cameraTarget = newLook.clone(); 
 }
 
 // Click 
 document.querySelector("button").addEventListener("click", () => {
     const targetPos = new THREE.Vector3(0, 0, 10);
-    const lookAt = new THREE.Vector3(0, 0, 0);
-    moveCameraTo(targetPos, lookAt);
+    moveCameraTo(targetPos);
 })
 
 document.getElementById("comp1").addEventListener("click", () => {
     const targetPos = new THREE.Vector3(5, 5, 5);
-    const lookAt = new THREE.Vector3(1, 0, 0);
-    moveCameraTo(targetPos, lookAt);
+    moveCameraTo(targetPos);
 })
 
 // Responsive resize
